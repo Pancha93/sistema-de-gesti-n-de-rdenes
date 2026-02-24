32d0f48384c4400f033374338255bde43846b788
pancha93 <alarroyavec_1@uqvirtual.edu.co>
2026-02-24 11:45:54 -0500

    link del backend

--- template/package/src/environments/environment.prod.ts
+++ template/package/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://tu-backend.onrender.com',
-  baseUrlApi: 'https://tu-backend.onrender.com/api',
-  baseUrlGitHub: 'https://tu-backend.onrender.com/authGitHub/github',
-  baseUrlRegistro: 'https://tu-backend.onrender.com/auth/crearUsuario',
+  baseUrl: 'https://sistema-de-gestion-de-ordenes-back-1.onrender.com',
+  baseUrlApi: 'https://sistema-de-gestion-de-ordenes-back-1.onrender.com/api',
+  baseUrlGitHub: 'https://sistema-de-gestion-de-ordenes-back-1.onrender.com/authGitHub/github',
+  baseUrlRegistro: 'https://sistema-de-gestion-de-ordenes-back-1.onrender.com/auth/crearUsuario',
   githubClientId: 'CLIENT_ID_PROD',
   redirectUri: 'https://tu-frontend.netlify.app/authentication/loginGithubCallback',
   googleClientId: 'CLIENT_ID_PROD_GOOGLE',
-  baseUrlExportar: 'https://tu-backend.onrender.com/api/generacion/exportar',
+  baseUrlExportar: 'https://sistema-de-gestion-de-ordenes-back-1.onrender.com/api/generacion/exportar',
 };
